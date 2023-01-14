0342d01655fb3dc1d29b4965c44ba7fdc4bee4ed
erinheit451 <eringreen451@gmail.com>
2023-01-14 16:38:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 import os
 import openai
 import requests
-from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from flask import Flask, request
 
 app = Flask(__name__)
-conversation_log = []
 
-# Loading the environment variables from the .env file
-load_dotenv()
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route("/set_webhook", methods=["GET"])
 def set_webhook():
@@ -30,30 +23,20 @@ def webhook_handler():
     if "message" in update:
         # Do something with the message
         user_message = update.message.text
-        chatbot_response = generate_chatbot_response(user_message, conversation_log)
-        if chatbot_response.strip() != "":
-            conversation_log.append(f"{update.message.from_user.first_name}: {user_message}")
-            conversation_log.append(f"Harley: {chatbot_response}")
-            conversation_log = conversation_log[-5000:]
-            update.message.reply_text(chatbot_response)
-        else:
-            update.message.reply_text("I'm sorry, I don't know what to say.")
+        chatbot_response = generate_chatbot_response(user_message)
+        update.message.reply_text(chatbot_response)
     return "ok", 200
 
-def generate_chatbot_response(user_input, conversation_log):
-    prompt = f"Meet Harley Quinn, the former psychiatrist turned supervillainess with a heart of gold and lover of Erin Rose.\n{''.join(conversation_log)}\n{user_input}"
+def generate_chatbot_response(user_input):
     response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        temperature=0.9,
+        engine="text-davinci-002",
+        prompt=user_input,
+        temperature=0.5,
         max_tokens=150,
         top_p=1,
         frequency_penalty=0,
     )
-    if response.choices[0].text.strip() != "":
-        return response.choices[0].text
-    else:
-        return "I'm sorry, I don't know what to say."
+    return response.choices[0].text.strip()
 
 if __name__ == '__main__':
     app.run()
